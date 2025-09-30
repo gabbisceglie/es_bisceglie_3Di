165bb1831608c8ba37dc4e2c307450ceaef09dce
Gabriele Bisceglie <bisceglie13200670@luigidellerba.edu.it>
2025-09-30 16:18:11 +0200

    es 4/6

---
+++ corso/esercizio4/esercizio4.java
@@ -0,0 +1,20 @@
+//Determinare area e perimetro di una circonferenza ricevendo in input la lunghezza del raggio
+
+import java.util.Scanner;
+
+public class esercizio4 {
+    public static void main(String[] args) {
+        
+    double r, a, p;
+
+    System.out.println("Inserisci il raggio della circonferenza: ");
+    Scanner scanner = new Scanner(System.in);
+    r = scanner.nextDouble();
+
+    a = Math.PI * r * r;
+    p = 2 * Math.PI * r;
+
+    System.out.println("Area: " + a);
+    System.out.println("Perimetro: " + p);
+    }
+}
---
+++ corso/esercizio5/esercizio5.java
@@ -0,0 +1,23 @@
+//Determinare area e perimetro di un rettangolo ricevendo in input base e altezza
+
+import java.util.Scanner;
+
+public class esercizio5 {
+    public static void main(String[] args) {
+
+        double base, altezza, area, perimetro;
+
+        System.out.println("Inserisci la base del rettangolo:");
+        Scanner scanner = new Scanner(System.in);
+        base = scanner.nextDouble();
+
+        System.out.println("Inserisci l'altezza del rettangolo:");
+        altezza = scanner.nextDouble();
+
+        area = base * altezza;
+        perimetro = 2 * (base + altezza);
+
+        System.out.println("L'area del rettangolo e': " + area);
+        System.out.println("Il perimetro del rettangolo e': " + perimetro);
+    }
+}
