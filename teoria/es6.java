2683c6acdbb284f93d0285dd339b809ce1ecc58e
Gabriele Bisceglie <bisceglie13200670@luigidellerba.edu.it>
2025-10-02 20:46:29 +0200

    teoria add

---
+++ teoria/es6.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class es6 {
+    public static void main(String[] args) {
+
+        //dichiara variabili
+        int a,b;
+
+        //crea scanner
+        Scanner scanner = new Scanner(System.in);
+
+        //richiede primo num
+        System.out.println("Inserisci un numero: ");
+        a = scanner.nextInt();
+
+        //richiede secondo num
+        System.out.println("Inserisci un altro numero: ");
+        b = scanner.nextInt();
+
+        //stampa somma
+        System.out.println("La somma dei due numeri è: " + (a+b));
+
+        //per evitare perdite di risorse
+        scanner.close();
+    }
+}
