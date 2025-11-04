3debb5b36b5189fa422d3e1ba4d1a9ef3c76e5a5
Gabriele Bisceglie <bisceglie13200670@luigidellerba.edu.it>
2025-11-04 08:56:42 +0100

    fix trova lo 0

--- C/trova_lo_0/trova_lo_0.c
+++ C/trova_lo_0/trova_lo_0.c
@@ -3,27 +3,33 @@
 int main() {
 
     int n, cont=0;
+
+    //input
+    do {
     printf("Inserisci un numero positivo: ");
     scanf("%d", &n);
+    } while(n<=0);
 
     int arr[n];
 
+    //input
     for(int i=0; i<n; i++) {
-        printf("Inserire un numero al valore " "%d: ", i);
+        printf("Inserire un numero al valore %d: ", i);
         scanf("%d", &arr[i]);
     }
 
+    //elaborazione
     for(int i=0; i<n; i++) {
         if(arr[i]==0){
-            printf("Lo zero e' contenuto al valore " "%d", i);
             cont++;
         }
     }
 
+    //output
     if(cont==0) {
         printf("\nNon sono stati inseriti 0 in nessun valore.");
     } else {
-        printf("\nGli zero contenuti, sono: " "%d: ", cont);
+        printf("\nGli zero contenuti, sono: %d ", cont);
     }
 
     return 0;
