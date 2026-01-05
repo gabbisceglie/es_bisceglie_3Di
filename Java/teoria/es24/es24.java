74ed149c5432b71bf6006af693ba5c76a5c9ec30
Gabriele Bisceglie <bisceglie13200670@luigidellerba.edu.it>
2026-01-05 17:12:12 +0100

    es25+ fix es24

--- Java/teoria/es24/es24.java
+++ Java/teoria/es24/es24.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class es1 {
+public class es24 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
--- Java/teoria/es25/es25.java
+++ Java/teoria/es25/es25.java
@@ -1,5 +1,50 @@
 package es25;
 
+import java.util.Scanner;
+
 public class es25 {
+    public static void main(String[] args) {
+        
+    Scanner scanner = new Scanner(System.in);
     
+    System.out.print("Inserisci la lunghezza dell'array: ");
+    int n = scanner.nextInt();
+
+    int[] array = new int[n];
+
+    generaArray(array);
+
+    System.out.println("Array generato:");
+    stampaArray(array);
+
+    sommaArray(array);
+
+        scanner.close();
+    }
+
+
+    //metodo per caricare l'array con valori generati casualmente
+    public static void generaArray (int arr[]){
+        for(int i=0; i<arr.length; i++){
+            arr[i] = (int)(Math.random()*100)+1;
+        }
+    }
+
+    //metodo per stampare l'array
+    public static void stampaArray(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    //metodo per calcolare e stampare la somma degli elementi dell'array
+    public static void sommaArray(int arr[]){
+        int somma = 0;
+        for(int i=0; i<arr.length; i++){
+            somma += arr[i];
+        }
+        System.out.println("La somma degli elementi dell'array è: " + somma);
+    }
+
 }
