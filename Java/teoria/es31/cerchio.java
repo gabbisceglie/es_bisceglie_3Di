2e6ca43ef3b27c1c3b749dbda37a9c7637fcd436
Gabriele Bisceglie <bisceglie13200670@luigidellerba.edu.it>
2026-01-29 10:26:57 +0100

    es31

---
+++ Java/teoria/es31/cerchio.java
@@ -0,0 +1,15 @@
+public class cerchio {
+
+    //attributo
+    private double raggio;
+
+    //metodo1
+    public void setRaggio(double r) {
+        raggio = r;
+    }
+
+    //metodo2
+    public double area() {
+        return Math.PI * raggio * raggio;
+    }
+}
---
+++ Java/teoria/es31/es31.java
@@ -0,0 +1,17 @@
+public class es31 {
+    public static void main(String[] args) {
+        
+        cerchio tavolo;
+
+        tavolo = new cerchio();
+
+        tavolo.setRaggio(0.75);
+
+        System.out.println("L'area del tavolo e': " + tavolo.area());
+
+
+
+
+
+    }
+}
